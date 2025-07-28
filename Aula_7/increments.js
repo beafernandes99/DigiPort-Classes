eb9a5ba57924bfa51da8e3f4ae7f783e476f1a37
BEATRIZ COSTA FERNANDES <beacfernandes@outlook.com>
2025-07-28 15:28:34 -0300

    aula 7 increments

---
+++ Aula_7/increments.js
@@ -0,0 +1,32 @@
+
+// let x = 10
+// x = x + 5
+// console.log(x)
+
+salario = 30
+horasTrabalhadas = 160
+salarioFinal= 30*160
+
+// console.log(salarioFinal)
+// console.log("salario final" + salarioFinal)
+
+// let salarioFinal = salario * horasTrabalhadas
+// console.log(salarioFinal)
+
+// precoOriginal = 200
+// desconto = preçoOriginal * 0.10
+// precoFinal = precoOriginal - desconto
+
+// console.log(precoFinal)
+
+// let idade = 17;
+// let maiorDeIdade = idade >= 18;
+// console.log(maiorDeIdade)
+
+let numero = 22;
+
+if (numero % 2 === 0) {
+  console.log("É par");
+} else {
+  console.log("É ímpar");
+}
