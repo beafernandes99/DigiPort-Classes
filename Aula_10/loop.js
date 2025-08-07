2b0d17c927070b12ca080fc74fa51dd798c45ada
BEATRIZ COSTA FERNANDES <beacfernandes@outlook.com>
2025-08-07 15:29:04 -0300

    aula 10 loop/for e while

---
+++ Aula_10/loop.js
@@ -0,0 +1,11 @@
+// Loop de 1 a 10
+for (let i = 1; i <= 10; i++) {
+  // Se o número for múltiplo de 3
+  if (i % 3 === 0) {
+    console.log("Donut " + i + ": recheio de chocolate");
+ } else {
+   // Se não for múltiplo de 3
+    console.log("Donut " + i + ": recheio de baunilha");
+  }
+}
+
